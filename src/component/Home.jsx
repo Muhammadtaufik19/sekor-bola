e6ee69f6ab41376ade0d2eaf648341eab1f710b0
Taufik <65449956+Muhammadtaufik19@users.noreply.github.com>
2021-07-09 22:21:54 +0700

    Change https

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -6,8 +6,7 @@ export default function Home() {
   const [loadding, setLoading] = useState(true);
 
   const ApiKey = "8592eae856c7437ab557faf50a45eabe";
-  const baseUrl =
-    " https://cors-everywhere.herokuapp.com/https://api.football-data.org/v2/";
+  const baseUrl = "https://api.football-data.org/v2/";
   const leagueId = "2021";
   const baseEndPoin = `${baseUrl}competitions/${leagueId}`;
   const teamEndPoin = `${baseUrl}competitions/${leagueId}/teams`;
